fbf2b2f4fd66bb5b2a7237f46aab40f1d37db405
HV-Armor <harrshvardhan308174@gmail.com>
2026-08-14 20:49:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort a String/sort-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    string sortString(string &s) {
+        // code here
+        vector<int>alpha(26,0);
+        for(int i=0;i<s.size();i++){
+            int index=s[i]-'a';
+            alpha[index]++;
+        }
+        string ans;
+        for(int i=0;i<26;i++){
+            char c='a'+i;
+            while(alpha[i]){
+                ans+=c;
+                alpha[i]--;
+            }
+        }
+        
+        return ans;
+    }
+};
